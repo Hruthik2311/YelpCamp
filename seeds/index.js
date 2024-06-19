dba7c09d4e827b22442242bb03c3b765066ae21d
Hruthik2311 <hruthikjallipalli2311@gmail.com>
2024-06-19 16:46:00 +0530

    second commit

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB= async()=>{
     await Campground.deleteMany({});
    for(let i=0;i<50;i++){
     const random1000=Math.floor(Math.random()*1000);
+    const price= Math.floor(Math.random()*20)+10;
    const camp= new Campground({
         location:`${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image:'https://images.unsplash.com/photo-1718354668365-993c89903d7c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, tempora voluptates adipisci facere cum laborum esse exercitationem maiores sapiente fuga qui, unde magnam quidem asperiores ratione distinctio repellat, reprehenderit fugiat',
+        price
     })
     await camp.save();
    }
