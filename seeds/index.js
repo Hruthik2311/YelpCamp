7dfa25684a5e0aff02d19073cad567f773455b8a
Hruthik2311 <hruthikjallipalli2311@gmail.com>
2024-08-10 16:54:46 +0530

    Changed connection in seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,12 @@ const Campground= require('../models/campground');
 const cities=require('./cities');
 const {places,descriptors}= require('./seedHelpers');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const dbUrl= process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp' ;
+
+mongoose.connect(dbUrl);
+
+// mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
 
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
