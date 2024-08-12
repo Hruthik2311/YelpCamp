582a4dc9d2802499df58b2351317195e07a27dc7
Hruthik2311 <hruthikjallipalli2311@gmail.com>
2024-08-12 10:48:30 +0530

    Adding seeds

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,8 @@ const Campground= require('../models/campground');
 const cities=require('./cities');
 const {places,descriptors}= require('./seedHelpers');
 
+require('dotenv').config();
+
 const dbUrl= process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp' ;
 
 mongoose.connect(dbUrl);
