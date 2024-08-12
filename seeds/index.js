562c48f8def2144c6e60e22df25863ee0cfb1026
Hruthik2311 <hruthikjallipalli2311@gmail.com>
2024-08-12 11:05:36 +0530

    author changes

--- seeds/index.js
+++ seeds/index.js
@@ -23,11 +23,11 @@ const sample =  array =>array[Math.floor(Math.random()* array.length)];
 
 const seedDB= async()=>{
     await Campground.deleteMany({});
-   for(let i=0;i<200;i++){
+   for(let i=0;i<50;i++){
     const random1000=Math.floor(Math.random()*1000);
     const price= Math.floor(Math.random()*20)+10;
    const camp= new Campground({
-        author: '66aa081276de2e5a1a285819',
+        author: '66b707ecec1a7270dea177a9',   //66aa081276de2e5a1a285819
         location:`${cities[random1000].city}, ${cities[random1000].state}`,
         title: `${sample(descriptors)} ${sample(places)}`,
         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, tempora voluptates adipisci facere cum laborum esse exercitationem maiores sapiente fuga qui, unde magnam quidem asperiores ratione distinctio repellat, reprehenderit fugiat',
